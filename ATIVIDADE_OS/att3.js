3d3daf821a52e25a14a57f755ac9a10cb38243e7
Raquel <raquelbulhoes212@gmail.com>
2024-04-05 11:07:17 -0300

    atividades os

---
+++ ATIVIDADE_OS/att3.js
@@ -0,0 +1,8 @@
+const os = require('os')
+
+
+    console.log('informações da memória:',os.totalmem(),'byte')
+    const conversao = (os.totalmem() / 1024)
+    console.log(conversao,'KiloByte')
+
+     
---
+++ ATIVIDADE_OS/att4.js
@@ -0,0 +1,4 @@
+const os = require('os')
+
+console.log(os.userInfo().username)
+console.log(os.userInfo().homedir)
